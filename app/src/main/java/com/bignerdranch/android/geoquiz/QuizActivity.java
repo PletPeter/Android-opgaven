0ab21fb5e3df73511d865c40cb78e5db4c5c9f46
PletPeter <peterjcorona@gmail.com>
2018-06-01 10:03:56 +0200

    donezo

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geoquiz;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +15,13 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
-    private Button mPreviousButton;
     private TextView mQuestionTextView;
+    private int score = 0;
+    private double percentage = 0;
+    private int answerCounter = 0;
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_australia, true),
@@ -31,7 +37,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = findViewById(R.id.question_text_view);
         updateQuestion();
@@ -41,6 +52,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
             }
         });
         mFalseButton = findViewById(R.id.false_button);
@@ -48,6 +61,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
             }
         });
 
@@ -57,21 +72,8 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
-            }
-        });
-
-        mPreviousButton = findViewById(R.id.previous_button);
-        mPreviousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mCurrentIndex>0) {
-                    mCurrentIndex = (mCurrentIndex - 1);
-                    updateQuestion();
-                }
-                else{
-                    mCurrentIndex = mQuestionBank.length - 1;
-                    updateQuestion();
-                }
+                mTrueButton.setEnabled(true);
+                mFalseButton.setEnabled(true);
             }
         });
 
@@ -80,10 +82,43 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
+                mTrueButton.setEnabled(true);
+                mFalseButton.setEnabled(true);
             }
         });
 
         updateQuestion();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onStart();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onStart();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(KEY_INDEX, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStart();
+        Log.d(TAG, "onStop() called");
     }
 
     private void updateQuestion() {
@@ -94,14 +129,40 @@ public class QuizActivity extends AppCompatActivity {
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
-        int messageResId = 0;
+        int messageResId;
 
         if (userPressedTrue == answerIsTrue){
             messageResId= R.string.correct_toast;
+            score++;
+            answerCounter++;
         } else {
             messageResId = R.string.incorrect_toast;
+            answerCounter++;
         }
-        makeToast(messageResId);
+        if(answerCounter!=6){
+            makeToast(messageResId);
+        }
+        else {
+            int percentage = 0;
+            switch (score){
+                case 0 : percentage = R.string.Score0_toast;
+                    break;
+                case 1 : percentage = R.string.Score1_toast;
+                    break;
+                case 2 : percentage = R.string.Score2_toast;
+                    break;
+                case 3 : percentage = R.string.Score3_toast;
+                    break;
+                case 4 : percentage = R.string.Score4_toast;
+                    break;
+                case 5 : percentage = R.string.Score5_toast;
+                    break;
+                case 6 : percentage = R.string.Score6_toast;
+            }
+            makeToast(percentage);
+            answerCounter = 0;
+            score = 0;
+        }
     }
 
     private void makeToast(int text) {
